0c84fe57c7d890202d343bc587a5b912734a7a35
Thiago <thaigoandroid@gmail.com>
2018-07-27 11:39:20 -0300

    remove import

--- dist/rss.js
+++ dist/rss.js
@@ -1,6 +1,20 @@
-import request from 'request';
-import { parseString } from 'xml2js';
-import { CONFIG } from './config';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _xml2js = require('xml2js');
+
+var _config = require('./config');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
  * Available RSS links
@@ -8,38 +22,41 @@ import { CONFIG } from './config';
  * @export
  * @class RSS
  */
-export default class RSS {
+var RSS =
 
-  /**
-   * Creates an instance of RSS.
-   * 
-   * @param {string} type 
-   * @param {any} callback 
-   * 
-   * @memberOf RSS
-   */
-  constructor(type, callback) {
-    const URL = `/${type}`;
-    const uri = `${CONFIG.BASE}${CONFIG.RSS}${URL}`;
+/**
+ * Creates an instance of RSS.
+ * 
+ * @param {string} type 
+ * @param {any} callback 
+ * 
+ * @memberOf RSS
+ */
+function RSS(type, callback) {
+  _classCallCheck(this, RSS);
 
-    request({ uri }, (error, response, body) => {
-      parseString(body, (err, result) => {
-        const length = result.rss.channel[0].item.length;
-        let rss = [];
+  var URL = '/' + type;
+  var uri = '' + _config.CONFIG.BASE + _config.CONFIG.RSS + URL;
 
-        for (let i = 0; i < length; i++) {
-          const obj = {
-            title       : result.rss.channel[0].item[i].title[0],
-            description : result.rss.channel[0].item[i].description[0],
-            link        : result.rss.channel[0].item[i].link[0],
-            date        : result.rss.channel[0].item[i].pubDate[0]
-          };
+  (0, _request2.default)({ uri: uri }, function (error, response, body) {
+    (0, _xml2js.parseString)(body, function (err, result) {
+      var length = result.rss.channel[0].item.length;
+      var rss = [];
 
-          rss.push(obj);
-        }
+      for (var i = 0; i < length; i++) {
+        var obj = {
+          title: result.rss.channel[0].item[i].title[0],
+          description: result.rss.channel[0].item[i].description[0],
+          link: result.rss.channel[0].item[i].link[0],
+          date: result.rss.channel[0].item[i].pubDate[0]
+        };
 
-        callback(rss, err);
-      });
+        rss.push(obj);
+      }
+
+      callback(rss, err);
     });
-  }
-}
+  });
+};
+
+exports.default = RSS;
